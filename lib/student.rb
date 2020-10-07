a2ca54d45ddf7df00ddecd011af94119ccf7355e
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-10-07 04:46:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,7 @@
 class Student
 
-attr_accessor :name :age 
+attr_accessor :name :age
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+  #  with DB[:conn]
+
 end
