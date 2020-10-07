14ff4a60e45c7c1fe50210a7225fac3e9951bfd7
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-10-07 04:58:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,9 +3,9 @@ class Student
 attr_accessor :name, :age, :id
 
 def initialize(name, age, id=nil)
+@id = id
 @name = name
 @age = age
-@id = id
 end   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
